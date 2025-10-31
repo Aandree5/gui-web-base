5a1f9a3598f89ed4f449a4fe4cf2d283b9fa1757
Andre Silva <32734153+Aandree5@users.noreply.github.com>
2025-10-31 16:40:46 +0000

    fix: argument parsing (#89)

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -22,7 +22,7 @@ while [ "$1" ]; do
         --no-restart)
             RESTART_FLAG="--no-restart"
             shift
-            ;;
+        ;;
         --title)
             shift
             if [ -z "$1" ]; then
@@ -32,8 +32,9 @@ while [ "$1" ]; do
             fi
             BROWSER_TITLE="$1"
             shift
-            ;;
+        ;;
         --min-quality)
+            shift
             if [ -z "$1" ]; then
                 echo "[ERROR] --min-quality requires a value."
                 echo "Usage: $0 [--min-quality <1-100>] <app_command> [args...]"
@@ -41,8 +42,9 @@ while [ "$1" ]; do
             fi
             MIN_QUALITY="$1"
             shift
-            ;;
+        ;;
         --min-speed)
+            shift
             if [ -z "$1" ]; then
                 echo "[ERROR] --min-speed requires a value."
                 echo "Usage: $0 [--min-speed <1-100>] <app_command> [args...]"
@@ -50,8 +52,9 @@ while [ "$1" ]; do
             fi
             MIN_SPEED="$1"
             shift
-            ;;
+        ;;
         --auto-refresh-delay)
+            shift
             if [ -z "$1" ]; then
                 echo "[ERROR] --auto-refresh-delay requires a value."
                 echo "Usage: $0 [--auto-refresh-delay <seconds>] <app_command> [args...]"
@@ -59,14 +62,14 @@ while [ "$1" ]; do
             fi
             AUTO_REFRESH_DELAY="$1"
             shift
-            ;;
+        ;;
         -*)
             echo "Unknown option: $1" >&2
             exit 2
         ;;
         *)
             break
-            ;;
+        ;;
     esac
 done
 
@@ -96,23 +99,23 @@ nginx -c "$NGINX_CONFIG" -g 'pid /gwb/nginx/nginx.pid;'
 echo "[INFO] Starting application: $APP_NAME"
 # (opengl=auto) - Disable OpenGL when not supported, like for alpine build for a smaller image (for OpenGL support use debian build)
 xpra seamless :100 \
-    --bind-tcp=127.0.0.1:3000 \
-    --ssl-cert=/gwb/ssl/ssl-cert.pem \
-    --html=on \
-    --exit-with-children=no \
-    --daemon=no \
-    --session-name="$BROWSER_TITLE" \
-    --socket-dirs="$XDG_RUNTIME_DIR" \
-    --window-close=ignore \
-    --opengl=auto \
-    --ssh=no \
-    --mdns=no \
-    --pulseaudio=yes \
-    --clipboard=yes \
-    --clipboard-direction=both \
-    --file-transfer=yes \
-    --webcam=no \
-    --min-quality=${MIN_QUALITY:-0} \
-    --min-speed=${MIN_SPEED:-0} \
-    --auto-refresh-delay=${AUTO_REFRESH_DELAY:-0.25} \
-    --start="watch-app $RESTART_FLAG -- \"$APP_CMD\""
+--bind-tcp=127.0.0.1:3000 \
+--ssl-cert=/gwb/ssl/ssl-cert.pem \
+--html=on \
+--exit-with-children=no \
+--daemon=no \
+--session-name="$BROWSER_TITLE" \
+--socket-dirs="$XDG_RUNTIME_DIR" \
+--window-close=ignore \
+--opengl=auto \
+--ssh=no \
+--mdns=no \
+--pulseaudio=yes \
+--clipboard=yes \
+--clipboard-direction=both \
+--file-transfer=yes \
+--webcam=no \
+--min-quality=${MIN_QUALITY:-0} \
+--min-speed=${MIN_SPEED:-0} \
+--auto-refresh-delay=${AUTO_REFRESH_DELAY:-0.25} \
+--start="watch-app $RESTART_FLAG -- \"$APP_CMD\""
